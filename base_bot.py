578a8c19739aeb8d44b47cddeb71701e19deafc2
jcaceresb <67157637+jcaceresb@users.noreply.github.com>
2020-07-13 10:46:28 -0500

    Update base_bot.py

--- base_bot.py
+++ base_bot.py
@@ -1,17 +1,17 @@
 import controles_bot
 
-def jugar_bot_leyenda(tab,val,val2):
+def jugar_bot_leyenda(tab,val,val2):   #Jugada del bot en modo "LEYENDA"
   print("Turno de la IA")
-  rpta, fila = controles_bot.jugada_bot(tab,val)
+  rpta, fila = controles_bot.jugada_bot(tab,val) #Busca jugada ganadora con sus fichas
   if rpta == True:
-    fila = fila + 1
+    fila = fila + 1 #Transforma el valor recibido a coordenadas para que juege el bot
   else:
-    rpta, fila = controles_bot.jugada_bot(tab,val2)
+    rpta, fila = controles_bot.jugada_bot(tab,val2) #Busca jugada para bloquear a jugador
     if rpta == True:
-      fila = fila + 1
+      fila = fila + 1 #Transforma el valor recibido a coordenadas para que juege el bot
     else:
       while True:
-        fila = controles_bot.jugada_bot2()
+        fila = controles_bot.jugada_bot2() #Numero aleatorio entre 1 a 7
         if tab[0][fila-1] == " ": #Si el tablero presenta  la casilla superior vacía (espacio para la ficha), acepta y rompe el while
           break
   for i in range(0,len(tab)): #Recorre el tablero
@@ -21,14 +21,14 @@ def jugar_bot_leyenda(tab,val,val2):
     elif i == 5:
       tab[i][fila-1] = val
 
-def jugar_bot_media(tab,val,val2):
+def jugar_bot_media(tab,val,val2):  #Jugada del bot en modo "MEDIA"
   print("Turno de la IA")
-  rpta, fila = controles_bot.jugada_bot(tab,val2)
+  rpta, fila = controles_bot.jugada_bot(tab,val2) #Busca jugada a bloquear dek jugador
   if rpta == True:
     fila = fila + 1
   else:
     while True:
-      fila = controles_bot.jugada_bot2()
+      fila = controles_bot.jugada_bot2()   #Numero aleatorio entre 1 y 7
       if tab[0][fila-1] == " ": #Si el tablero presenta  la casilla superior vacía (espacio para la ficha), acepta y rompe el while
         break
   for i in range(0,len(tab)): #Recorre el tablero
