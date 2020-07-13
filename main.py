cb12f1bc1fa71e5d77fa952924ac4bb20a0915ae
jcaceresb <67157637+jcaceresb@users.noreply.github.com>
2020-07-12 20:38:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,128 +1,247 @@
-def mostrar(x):
+import controles   #Importa las funciones de "controles.py"
+import base_bot    #Importa las funciones de "base_bot.py"
+
+def mostrar(x,y):
   for i in range(0,len(x)): #Imprime el tablero igual a tablero de connect 4
     print(x[i])
-
-def comprobar(t,n):
-  while True: #Para que continue comprobando co todas las opciones
-    cond1 = False #La condicion que cambiará si hay una jugada ganadora
-    for i in range(0,len(t)): #Comprobación horizontal
-      for fila in range(0,4):
-        if t[i][fila] == n and t[i][fila+1] == n and t[i][fila+2] == n and t[i][fila+3] == n: #Posiciones
-          cond1 = True #Si gana, cambia la condicion y termina el for
-          break
-
-    if cond1 == True:
-      break #Si gana, termina el while general
-  
-    for i in range(0,7): #Comprobación vertical
-      for col in range(0,3):
-       if t[col][i] == n and t[col+1][i] == n and t[col+2][i] == n and t[col+3][i] == n: #Posiciones
-          cond1 = True
-          break
-
-    if cond1 == True:
-     break
-
-    for i in range(0,3): #Comprobación en diagonal 1
-      for diag in range(0,4):
-        if t[i][diag] == n and t[i+1][diag+1] == n and t[i+2][diag+2] == n and t[i+3][diag+3] == n: #Posiciones
-          cond1 = True
-          break
-      
-    if cond1 == True:
-      break
-
-    for i in range(0,3): #Comrpobación en diagonal 2
-      for diag in range(3,7):
-        if t[i][diag] == n and t[i-1][diag-1] == n and t[i-2][diag-2] == n and t[i-3][diag-3] == n: #Posiciones
-          cond1 = True
-          break
-
-    break
-  return cond1 #Devuelve el valor de "cond1"
+  print(y)
 
 def jugar(jug,val): #Para cada turno de jugador
   preg = jug + ", introduzca un número (1-7): " #Define la pregunta usando el nombre
   while True: #Ciclo infinito para obligar al jugador a tener una jugada válida
     num = int(input(preg)) #Pregunta el lugar de la casilla
-    if tablero[0][num-1] == 0: #Si el tablero presenta la casilla superior vacía (espacio para la ficha), acepta y rompe el while
+    if tablero[0][num-1] == " ": #Si el tablero presenta la casilla superior vacía (espacio para la ficha), acepta y rompe el while
       break
   for i in range(0,len(tablero)): #Recorre el tablero
-    if tablero[i][num-1] != 0: # Cae hasta que encuentra una ficha y se pone un sitio anterior
+    if tablero[i][num-1] != " ": # Cae hasta que encuentra una ficha y se pone un sitio anterior
       tablero[i-1][num-1] = val
       break
     elif i == 5:
       tablero[i][num-1] = val #Cae hasta el final del tablero si la columna está vacía
 
-def tablero_lleno(t): #Funcion para definir el tablero lleno
-  lleno = True
-  for i in range(0,len(t)): #Recorre todo el tablero
-    for j in range(0,7):
-      if t[i][j] == 0: #Si hay al menos un hueco, indica que no está lleno
-        lleno = False
-        break
-  return lleno #devuelve el valor de lleno
-
 print("BENVENIDOS A CONNECT 4") #Introduccion del juego
 print()
 print("Hecho por: Jose Caceres, Augusto Bracamonte y Vasco Bustamante")
 print()
-jugador1 = input("Jugador 1, introduce tu nombre: ") #Introducción del nombre de los jugadores
+print("MODOS DE JUEGO:")
 print()
-jugador2 = input("Jugador 2, introduce tu nombre: ")
+print("JUGADOR VS IA: 1")
+print("JUGADOR VS JUGADOR: 2")
 print()
-
-tablero = [[0,0,0,0,0,0,0], #Define el tablero
-[0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0]]
-
-mostrar(tablero) #Muestra el tablero inicial
+while True:
+  modo = int(input("¿Qué modo jugarás? (1-2): "))
+  if modo == 1 or modo == 2:
+    break
 print()
 
-while True: #Turnos infinitos hasta que alguien gane
+tablero = [[" "," "," "," "," "," "," "], #Define el tablero
+[" "," "," "," "," "," "," "],
+[" "," "," "," "," "," "," "],
+[" "," "," "," "," "," "," "],
+[" "," "," "," "," "," "," "],
+[" "," "," "," "," "," "," "]]
+
+numeros = ["1","2","3","4","5","6","7"]
+
+if modo == 2:
+  print("Has seleccionado JUGADOR VS JUGADOR")
+  print()
+  jugador1 = input("Jugador 1, introduce tu nombre: ") #Introducción del nombre de los jugadores
+  print()
+  jugador2 = input("Jugador 2, introduce tu nombre: ")
+  print()
+
+  mostrar(tablero,numeros) #Muestra el tablero inicial
+  print()
+
+  cont = 0   #Variable que contará el número de turnos hasta el final del juego
+
+  while True: #Turnos infinitos hasta que alguien gane
   
-  jugar(jugador1,1) #Turno del jugador 1 con ficha "1"
+    jugar(jugador1,"o") #Turno del jugador 1 con ficha "1"
 
+    print()
+
+    mostrar(tablero,numeros) #Muestra el tablero luego de la jugada
+
+    comprobacion = controles.comprobar(tablero,"o") #Comprueba con las fichas del jugador 1
+    if comprobacion == True: 
+      ganador = jugador1 #Si gana, declara al jugador 1 como ganador y se rompe el while
+      break
+
+    tab_lleno = controles.tablero_lleno(tablero) #Comprueba si el tablero esta lleno
+    if tab_lleno == True:
+      ganador = "Empate" #Si el tablero esta lleno declara al ganadorcomo empate y rompe el while
+      break
+
+    print()
+
+    jugar(jugador2,"+") #Turno del jugador 2 con ficha "2"
+
+    print()
+
+    mostrar(tablero,numeros) #Muestra el tablero luego de la jugada
+
+    comprobacion = controles.comprobar(tablero,"+") #Comprueba con las fichas del jugador 2
+    if comprobacion == True: 
+      ganador = jugador2 #Si gana, declara al jugador 2 como ganador y se rompe el while
+      break
+
+    tab_lleno = controles.tablero_lleno(tablero) #Comprueba si el tablero esta lleno
+    if tab_lleno == True:
+      ganador = "Empate" #Si el tablero esta lleno declara al ganador como empate y rompe el while
+      break
+
+    print()
+
+else:
+  print("Has seleccionado JUGADOR VS IA")
+  print()
+  print("DIFICULTAD:")
+  print()
+  print("MEDIA: 1")
+  print("LEYENDA: 2")
+  print()
+  while True:
+    dificultad = int(input("¿Qué modo jugarás? (1-2): "))
+    if dificultad == 1 or dificultad == 2:
+        break
   print()
 
-  mostrar(tablero) #Muestra el tablero luego de la jugada
+  if dificultad == 2:
+    print("Has seleccionado DIFICULTAD LEYENDA")
+    print()
+    jugador1 = input("Jugador 1, introduce tu nombre: ") #Introducción del nombre de los jugadores
+    print()
 
-  comprobacion = comprobar(tablero,1) #Comprueba con las fichas del jugador 1
-  if comprobacion == True: 
-    ganador = jugador1 #Si gana, declara al jugador 1 como ganador y se rompe el while
-    break
+    mostrar(tablero,numeros) #Muestra el tablero inicial
+    print()
 
-  tab_lleno = tablero_lleno(tablero) #Comprueba si el tablero esta lleno
-  if tab_lleno == True:
-    ganador = "Empate" #Si el tablero esta lleno declara al ganador como empate y rompe el while
-    break
+    while True: #Turnos infinitos hasta que alguien gane
+  
+      jugar(jugador1,"o") #Turno del jugador 1 con ficha "o"
+      
+      print()
 
-  print()
+      mostrar(tablero,numeros) #Muestra el tablero luego de la jugada
 
-  jugar(jugador2,2) #Turno del jugador 2 con ficha "2"
+      comprobacion = controles.comprobar(tablero,"o") #Comprueba con las fichas del jugador 1
+      if comprobacion == True: 
+        ganador = jugador1 #Si gana, declara al jugador 1 como ganador y se rompe el while
+        break
 
-  print()
+      tab_lleno = controles.tablero_lleno(tablero) #Comprueba si el tablero esta lleno
+      if tab_lleno == True:
+        ganador = "Empate" #Si el tablero esta lleno declara al ganador como empate y rompe el while
+        break
 
-  mostrar(tablero) #Muestra el tablero luego de la jugada
+      print()
 
-  comprobacion = comprobar(tablero,2) #Comprueba con las fichas del jugador 2
-  if comprobacion == True: 
-    ganador = jugador2 #Si gana, declara al jugador 2 como ganador y se rompe el while
-    break
+      base_bot.jugar_bot_leyenda(tablero,"+","o") #Turno de la IA con ficha "+"
 
-  tab_lleno = tablero_lleno(tablero) #Comprueba si el tablero esta lleno
-  if tab_lleno == True:
-    ganador = "Empate" #Si el tablero esta lleno declara al ganador como empate y rompe el while
-    break
+      print()
 
-  print()
+      mostrar(tablero,numeros) #Muestra el tablero luego de la jugada
+
+      comprobacion = controles.comprobar(tablero,"+") #Comprueba con las fichas del jugador 2
+      if comprobacion == True: 
+        ganador = "IA" #Si gana, declara a la IA como ganadora y se rompe el while
+        break
+
+      tab_lleno = controles.tablero_lleno(tablero) #Comprueba si el tablero esta lleno
+      if tab_lleno == True:
+        ganador = "Empate" #Si el tablero esta lleno declara al ganador como empate y rompe el while
+        break
+
+      print()
+
+  else:
+    print("Has seleccionado DIFICULTAD MEDIA")
+    print()
+    jugador1 = input("Jugador 1, introduce tu nombre: ") #Introducción del nombre de los jugadores
+    print()
+
+    mostrar(tablero,numeros) #Muestra el tablero inicial
+    print()
+
+    cont = 0   #Variable que contará el número de turnos hasta el final del juego
+
+    while True: #Turnos infinitos hasta que alguien gane
+  
+      jugar(jugador1,"o") #Turno del jugador 1 con ficha "o"
+
+      print()
+
+      mostrar(tablero,numeros) #Muestra el tablero luego de la jugada
+
+      cont = cont + 1
+
+      comprobacion = controles.comprobar(tablero,"o") #Comprueba con las fichas del jugador 1
+      if comprobacion == True: 
+        ganador = jugador1 #Si gana, declara al jugador 1 como ganador y se rompe el while
+        break
+
+      tab_lleno = controles.tablero_lleno(tablero) #Comprueba si el tablero esta lleno
+      if tab_lleno == True:
+        ganador = "Empate" #Si el tablero esta lleno declara al ganador como empate y rompe el while
+        break
+
+      print()
+
+      base_bot.jugar_bot_media(tablero,"+","o") #Turno de la IA con ficha "+"
+
+      print()
+
+      mostrar(tablero,numeros) #Muestra el tablero luego de la jugada
+
+      comprobacion = controles.comprobar(tablero,"+") #Comprueba con las fichas del jugador 2
+      if comprobacion == True: 
+        ganador = "IA" #Si gana, declara a la IA como ganadora y se rompe el while
+        break
+
+      tab_lleno = controles.tablero_lleno(tablero) #Comprueba si el tablero esta lleno
+      if tab_lleno == True:
+        ganador = "Empate" #Si el tablero esta lleno declara al ganador como empate y rompe el while
+        break
+
+      print()
 
 print()
 print("JUEGO TERMINADO") #Termina el juego
 print()
 print("Ganador:",ganador) #Se menciona al jugador ganador
+print()
+
+if modo == 1:
+  if dificultad == 1:
+    if ganador != "IA":
+      print("FELICIDADES, ESTAS EN EL TOP")
+      controles.rank_media(ganador,cont)
+    print()
+    print("MEJORES PUNTUACIONES (DIFICULTAD MEDIA)")
+    print()
+    print("Gana en el menor número de jugadas para aparecer")
+    print()
+    archivo3 = open("ranking_media.txt","r")
+    for puesto in archivo3:
+      datos = puesto.split(" ")
+      print("Nombre: " + datos[0], end="   ")
+      print("Puntaje: "+(str(int(datos[1])))+"\n")
+    archivo3.close()
+  else:
+    if ganador != "IA":
+      print("FELICIDADES, GANASTE A LA IA EN MODO LEYENDA")
+      controles.rank_leyen(ganador)
+    print("GANADORES (DIFICULTAD LEYENDA)")
+    print()
+    print("Gana en dificultad leyenda para aparecer")
+    print()
+    archivo3 = open("ranking_leyenda.txt","r")
+    for puesto in archivo3:
+      datos = puesto.split(" ")
+    archivo3.close()
+    for i in range(0,len(datos)):
+      print("Nombre: " + datos[i])
+
 print()
 print("GRACIAS POR JUGAR") #Gracias por jugar y escuchar nuestra explicación :D
